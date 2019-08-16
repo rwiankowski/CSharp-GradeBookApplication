647cb5f1a4da90e184160054a17e201a520596d6
Radoslaw Wiankowski <rwiankowski@schubergphilis.com>
2019-08-16 08:40:14 +0200

    Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,5 +15,29 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        override public char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count() < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least five students!");
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count() * 0.2);
+
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[threshold * 2 - 1] <= averageGrade)
+                return 'B';
+            else if (grades[threshold * 3 - 1] <= averageGrade)
+                return 'C';
+            else if (grades[threshold * 4 - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+
+        }
     }
 }
